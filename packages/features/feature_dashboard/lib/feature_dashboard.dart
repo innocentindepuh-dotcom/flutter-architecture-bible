c294728e0463653a44d73b655616d3b7e432db79
Nexa Admin <developer@users.noreply.github.com>
2026-09-21 10:28:50 +0100

    feat: initialize dashboard feature package and configuration

---
+++ packages/features/feature_dashboard/lib/feature_dashboard.dart
@@ -0,0 +1,5 @@
+﻿library feature_dashboard;
+
+export "src/domain/entities/saas_metric_entity.dart";
+export "src/domain/repositories/dashboard_repository.dart";
+export "src/presentation/screens/saas_dashboard_screen.dart";
