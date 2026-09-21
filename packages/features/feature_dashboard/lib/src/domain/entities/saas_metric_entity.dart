e3b48579f456604c53d7739a61cc2b1717db37dc
Nexa Admin <developer@users.noreply.github.com>
2026-09-21 10:31:11 +0100

    feat: implement dashboard domain entities, repository contracts, and pubspec config

---
+++ packages/features/feature_dashboard/lib/src/domain/entities/saas_metric_entity.dart
@@ -0,0 +1,31 @@
+﻿// packages/features/feature_dashboard/lib/src/domain/entities/saas_metric_entity.dart
+
+class SaasMetricEntity {
+  final String metricName;
+  final double value;
+  final double changePercentage;
+
+  const SaasMetricEntity({
+    required this.metricName,
+    required this.value,
+    required this.changePercentage,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SaasMetricEntity &&
+          runtimeType == other.runtimeType &&
+          metricName == other.metricName &&
+          value == other.value &&
+          changePercentage == other.changePercentage;
+
+  @override
+  int get hashCode =>
+      metricName.hashCode ^ value.hashCode ^ changePercentage.hashCode;
+
+  @override
+  String toString() =>
+      "SaasMetricEntity(name: $metricName, value: $value, change: $changePercentage%)";
+}
+
---
+++ packages/features/feature_dashboard/lib/src/domain/repositories/dashboard_repository.dart
@@ -0,0 +1,8 @@
+﻿// packages/features/feature_dashboard/lib/src/domain/repositories/dashboard_repository.dart
+
+import "../entities/saas_metric_entity.dart";
+
+abstract class DashboardRepository {
+  Stream<List<SaasMetricEntity>> watchLiveMetrics();
+}
+
